fc88ddf4f0912d15aa174487b44f1faa2630c1f9
pravesh <pravesh.meena2892@gmail.com>
2024-02-10 22:55:43 +0530

    final update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import 'tailwindcss/tailwind.css';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(
+          'https://randomuser.me/api/?page=1&results=1&seed=abc'
+        );
+        console.log(response)
+        setUser(response.data.results[0]);
+      } catch (error) {
+        console.error('Error fetching user:', error);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="flex justify-center items-center h-screen">
+    {user && (
+      <div className="flex max-w-4xl bg-white rounded-md overflow-hidden shadow-lg p-10">
+        <div className="mr-6">
+          <img
+            src={user.picture.large}
+            alt="Profile"
+            className="w-40 h-40 object-cover rounded-full"
+          />
+        </div>
+        <div className="flex flex-col">
+          <h2 className="text-xl font-bold mb-2 text-center">
+            {user.name.first} {user.name.last}
+          </h2>
+          <p className="text-gray-600 mb-2 text-center">{user.gender}</p>
+          <p className="text-gray-600 mb-2 text-center">{user.phone}</p>
+        </div>
+      </div>
+    )}
+  </div>
   );
 }
 
 export default App;
+
